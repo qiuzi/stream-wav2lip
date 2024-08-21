44ccedb72aa70572303872c3aaaa145a01674cd2
qiuzi <gxfclql@gmail.com>
2024-08-21 12:06:42 +0800

    Update sam.py
    1280

--- primepake_wav2lip/models/sam.py
+++ primepake_wav2lip/models/sam.py
@@ -31,123 +31,128 @@ class SAM(nn.Module):
         return out
 
 
-class Wav2Lip_192SAM(nn.Module):
-    def __init__(self, audio_encoder=None):
-        super(Wav2Lip_192SAM, self).__init__()
+
+class Wav2Lip_1280SAM(nn.Module):
+    def __init__(self, audio_encoder=None, is_sam=True):
+        super(Wav2Lip_1280SAM, self).__init__()
+        self.is_sam = is_sam
         self.sam = SAM()
+        
         self.face_encoder_blocks = nn.ModuleList([
-            nn.Sequential(Conv2d(6, 16, kernel_size=7, stride=1, padding=3),
-                          Conv2d(16, 16, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(16, 16, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(16, 16, kernel_size=3, stride=1, padding=1, residual=True)),  # 192, 192
+            nn.Sequential(Conv2d(6, 16, kernel_size=7, stride=1, padding=3, act="leaky")),  # 384, 384
 
-            nn.Sequential(Conv2d(16, 32, kernel_size=3, stride=2, padding=1),  # 96, 96
-                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True)),
+            nn.Sequential(Conv2d(16, 32, kernel_size=5, stride=(1, 2), padding=1, act="leaky"),
+                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 384, 192
 
-            nn.Sequential(Conv2d(32, 64, kernel_size=3, stride=2, padding=1),  # 48, 48
-                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True)),
+            nn.Sequential(Conv2d(32, 64, kernel_size=5, stride=2, padding=1, act="leaky"),
+                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 192, 96
 
-            nn.Sequential(Conv2d(64, 128, kernel_size=3, stride=2, padding=1),  # 24, 24
-                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True)),
+            nn.Sequential(Conv2d(64, 128, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 96, 48
 
-            nn.Sequential(Conv2d(128, 256, kernel_size=3, stride=2, padding=1),  # 12, 12
-                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True)),
+            nn.Sequential(Conv2d(128, 256, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 48, 24
 
-            nn.Sequential(Conv2d(256, 512, kernel_size=3, stride=2, padding=1),  # 6, 6
-                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True)),
+            nn.Sequential(Conv2d(256, 512, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 24, 12
 
-            nn.Sequential(Conv2d(512, 1024, kernel_size=3, stride=2, padding=1),  # 3, 3
-                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True)),
+            nn.Sequential(Conv2d(512, 768, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 12, 6
 
-            nn.Sequential(Conv2d(1024, 1024, kernel_size=3, stride=1, padding=0),  # 1, 1
-                          Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0),
-                          Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0)), ])
-        ##################
+            nn.Sequential(Conv2d(768, 1024, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 6, 3
+
+            nn.Sequential(Conv2d(1024, 1280, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(1280, 1280, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(1280, 1280, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),  # 3, 2
+
+            nn.Sequential(Conv2d(1280, 1280, kernel_size=3, stride=2, padding=1, act="leaky"),
+                          Conv2d(1280, 1280, kernel_size=3, stride=1, padding=0, act="leaky"),
+                          Conv2d(1280, 1280, kernel_size=1, stride=1, padding=0, act="relu"))  # 1, 1
+        ])
 
         if audio_encoder is None:
             self.audio_encoder = nn.Sequential(
-                Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
-                Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True),
-                Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True),
+                Conv2d(1, 32, kernel_size=3, stride=1, padding=1, act="leaky"),
+                Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
 
-                Conv2d(32, 64, kernel_size=3, stride=(3, 1), padding=1),
-                Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
-                Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
+                Conv2d(32, 64, kernel_size=3, stride=(3, 1), padding=1, act="leaky"),
+                Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
 
-                Conv2d(64, 128, kernel_size=3, stride=3, padding=1),
-                Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
-                Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
+                Conv2d(64, 128, kernel_size=3, stride=3, padding=1, act="leaky"),
+                Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
 
-                Conv2d(128, 256, kernel_size=3, stride=(3, 2), padding=1),
-                Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
-                Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
+                Conv2d(128, 256, kernel_size=3, stride=(3, 2), padding=1, act="leaky"),
+                Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
 
-                ###################
-                # Modified blocks
-                ##################
-                Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-                Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),
-                Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),
+                Conv2d(256, 512, kernel_size=3, stride=1, padding=1, act="leaky"),
+                Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
 
-                Conv2d(512, 1024, kernel_size=3, stride=1, padding=0),
-                Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0))
-            ##################
+                Conv2d(512, 768, kernel_size=3, stride=1, padding=1, act="leaky"),
+                Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+
+                Conv2d(768, 1024, kernel_size=3, stride=1, padding=1, act="leaky"),
+                Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+
+                Conv2d(1024, 1280, kernel_size=3, stride=1, padding=0, act="relu"),
+                Conv2d(1280, 1280, kernel_size=1, stride=1, padding=0, act="relu")
+            )
         else:
             self.audio_encoder = audio_encoder
-
             for p in self.audio_encoder.parameters():
                 p.requires_grad = False
-        self.audio_refine = nn.Sequential(
-            Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0),
-            Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0))
 
         self.face_decoder_blocks = nn.ModuleList([
-            nn.Sequential(Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0), ),  # + 1024
+            nn.Sequential(Conv2d(1280, 1280, kernel_size=1, stride=1, padding=0, act="relu")),
 
-            ###################
-            # Modified blocks
-            ##################
-            nn.Sequential(Conv2dTranspose(2048, 1024, kernel_size=3, stride=1, padding=0),  # 3,3
-                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True), ),  # + 1024
+            nn.Sequential(Conv2dTranspose(2560, 1280, kernel_size=3, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(1280, 1280, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(1280, 1280, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-            nn.Sequential(Conv2dTranspose(2048, 1024, kernel_size=3, stride=2, padding=1, output_padding=1),
-                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True), ),  # 6, 6  + 512
+            nn.Sequential(Conv2dTranspose(2560, 1024, kernel_size=3, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-            nn.Sequential(Conv2dTranspose(1536, 768, kernel_size=3, stride=2, padding=1, output_padding=1),
-                          Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True), ),  # 12, 12  + 256
+            nn.Sequential(Conv2dTranspose(1792, 768, kernel_size=3, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(768, 768, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-            nn.Sequential(Conv2dTranspose(1024, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
-                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True), ),  # 24, 24  + 128
-            ##################
+            nn.Sequential(Conv2dTranspose(1280, 512, kernel_size=3, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(512, 512, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-            nn.Sequential(Conv2dTranspose(640, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
-                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True), ),  # 48, 48  + 64
+            nn.Sequential(Conv2dTranspose(768, 256, kernel_size=3, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(256, 256, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-            nn.Sequential(Conv2dTranspose(320, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
-                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True), ),  # 96, 96  + 32
+            nn.Sequential(Conv2dTranspose(384, 128, kernel_size=3, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(128, 128, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-            nn.Sequential(Conv2dTranspose(160, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
-                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True),
-                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True), ), ])  # 192, 192  + 16
+            nn.Sequential(Conv2dTranspose(192, 64, kernel_size=5, stride=2, padding=1, output_padding=1, act="leaky"),
+                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(64, 64, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
 
-        self.output_block = nn.Sequential(Conv2d(80, 32, kernel_size=3, stride=1, padding=1),
+            nn.Sequential(Conv2dTranspose(96, 32, kernel_size=5, stride=(1, 2), padding=1, output_padding=(0, 1), act="leaky"),
+                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True, act="leaky"),
+                          Conv2d(32, 32, kernel_size=3, stride=1, padding=1, residual=True, act="leaky")),
+        ])
+
+        self.output_block = nn.Sequential(Conv2d(48, 32, kernel_size=3, stride=1, padding=1, act="leaky"),
                                           nn.Conv2d(32, 3, kernel_size=1, stride=1, padding=0),
                                           nn.Sigmoid())
 
